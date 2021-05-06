31342a288cd23ed165405d5c4a7486a8baa7367d
estherxin13 <56760895+estherxin13@users.noreply.github.com>
2021-05-06 18:29:22 -0400

    styling the app and adding images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { React, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
-import Image from './images/forrest.png'; // Import using relative path
-import Card from '@material-ui/core/Card';
 import Input from '@material-ui/core/Input';
+import { Divider, Grid } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Image from './images/nature.png'; // Import using relative path
 
 
 const api = {
@@ -19,36 +20,35 @@ const useStyles = makeStyles(theme => ({
       minWidth: '50ch',
     },
   },
+  main: {
+    minHeight: '100vh',
+    paddingTop: theme.spacing(4),
+  },
   app: {
-    // backgroundImage: `url(${Image})`,
-    // backgroundColor: 'black',
-    // backgroundSize: 'cover',
-    // backgroundPosition: 'top',
-    textAlign: 'center',
+    backgroundImage: `url(${Image})`,
     [theme.breakpoints.down('md')]: {
       backgroundImage: 'none',
-      backgroundColor: 'black',
     },
     
   },
-  main: {
-    minHeight: '100vh',
-    paddingTop: theme.spacing(10),
-
-  },
   card: {
-    width: 500,
+    backgroundColor: 'white',
+    width: 700,
     margin: 'auto',
     minHeight: 100,
-    padding: theme.spacing(5),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
   },
   location: {
-    margin: theme.spacing(5),
-    fontSize: 25,
-    textShadow: '2px 2px grey',
+    fontSize: 30,
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(6),
   },
   date: {
-    margin: theme.spacing(5),
+    marginTop: theme.spacing(5),
+    color: 'grey',
     fontSize: 18,
   },
   temp: {
@@ -56,8 +56,9 @@ const useStyles = makeStyles(theme => ({
     fontSize: 30,
   },
   weather: {
-    margin: theme.spacing(5),
-    fontSize: 18,
+    margin: theme.spacing(3),
+    fontSize: 35,
+    textAlign: 'center'
   },
   search: {
     paddingTop: theme.spacing(2),
@@ -67,6 +68,35 @@ const useStyles = makeStyles(theme => ({
     outline: 'none',
     display: 'block',
     borderRadius: 6,
+  },
+  imageCloud: {
+    width: 400,
+  },
+  imageSun: {
+    width: 200,
+  },
+  imageRain: {
+    width: 250,
+  },
+  center: {
+    textAlign: 'center'
+  },
+  textRoot: {
+    textAlign: 'center',
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(2),
+    '& ul': {
+      listStyle: 'none',
+      padding: 0,
+    },
+    '& li': {
+      paddingTop: theme.spacing(1),
+    },
+  },
+  subtitle: {
+    color: 'grey',
+    fontSize: 20,
+    marginTop: theme.spacing(1),
   }
 
 }));
@@ -89,7 +119,6 @@ function App() {
     }
   }
 
-
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -100,16 +129,12 @@ function App() {
     let year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`
-
   }
 
   return (
     <div className={classes.app}>
       <main className={classes.main}>
-        <Card className={classes.card}>
-          {/* <div className={classes.title}>
-            Hey welcome to my weather app!
-          </div> */}
+        <Box boxShadow={3} className={classes.card}>
           <Input
             variant="outlined"
             className={classes.search}
@@ -120,28 +145,70 @@ function App() {
             placeholder="search any city"
           />
           {(typeof weather.main != "undefined") && (
-            <div className={classes.locationBox}>
-              <div className={classes.location}><strong>{weather.name}, {weather.sys.country}</strong></div>
-              <div className={classes.date}><strong>Date: </strong>{dateBuilder(new Date())}</div>
-              <div className={classes.temp}>
-                {weather.main.temp} °C
+            <>
+              <div className={classes.date}>
+                {dateBuilder(new Date())}
               </div>
+              <div className={classes.location}>
+                <strong>{weather.name}, {weather.sys.country}</strong>
+              </div>
+              {weather.weather[0].main === "Rain" && 
+              <div className={classes.center}>
+                <img className={classes.imageRain} alt="logo" src="/rain.png" />
+              </div>}
+              {weather.weather[0].main === "Sunny" || weather.weather[0].main === "Clear" ?
+              <div className={classes.center}>
+                <img className={classes.imageSun} alt="logo" src="/sun.png" />
+              </div>: <></>}
+              {weather.weather[0].main === "Clouds" && 
+              <div className={classes.center}>
+                <img className={classes.imageCloud} alt="logo" src="/cloud.png" />
+              </div>}
               <div className={classes.weather}>
                 {weather.weather[0].main}
               </div>
-              <div className={classes.temp}>
-                feels like: {weather.main.feels_like} °C
+              <div className={classes.textRoot}>
+              <Divider />
+                <Grid container spacing={0} justifyContent="space-around">
+                  <Grid item xs={12} sm={4}>
+                    <ul>
+                      <li className={classes.subtitle}>
+                        humidity
+                      </li>
+                      <li className={classes.weather}>
+                        <strong>{weather.main.humidity} %</strong>
+                      </li>
+                    </ul>
+                  </Grid>
+                  <Divider orientation="vertical" flexItem />
+                  <Grid item xs={12} sm={4}>
+                    <ul>
+                      <li className={classes.subtitle}>
+                        temp
+                      </li>
+                      <li>
+                        <div className={classes.weather}>
+                          <strong>{Math.round(weather.main.temp)} °C</strong>
+                        </div>
+                      </li>
+                    </ul>
+                  </Grid>
+                  <Divider orientation="vertical" flexItem />
+                  <Grid item xs={12} sm={3}>
+                    <ul>
+                      <li className={classes.subtitle}>
+                        wind
+                      </li>
+                      <li className={classes.weather}>
+                        <strong>{weather.wind.speed}</strong>
+                      </li>
+                    </ul>
+                  </Grid>
+                </Grid>
               </div>
-              {/* {weather.weather[0].main === "Rain" && <div>hey</div>} */}
-              <div className={classes.temp}>
-                humidity: {weather.main.humidity} %
-              </div>
-              <div className={classes.temp}>
-                wind speed: {weather.wind.speed} km/h
-              </div>
-            </div>
+            </>
           )}
-        </Card>
+        </Box>
       </main>
     </div>
   );
